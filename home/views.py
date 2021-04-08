f1339d6d3c5e35645642b03743044036ffebdba4
ttrssreal <afegfdj5wrtggvaq13r@protonmail.com>
2021-04-09 11:01:39 +1200

    Inital

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-home_blueprint = Blueprint("home",__name__,template_folder="templates")
-
-@home_blueprint.route("/")
-@home_blueprint.route("/home")
-def GET_home():
-    return render_template("index.html")
